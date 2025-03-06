d32ebdc1d38bc1c2e7d8fee111c26b40c1f0ee33
Nossnoj <tjonsson02@gmail.com>
2025-03-06 21:03:38 +0100

    Ändrat lite skit

--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using System.Dynamic;
 using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -12,8 +13,8 @@ namespace PasswordManager
     {
         static void Main(string[] args)
         {
-
-            attemptDecryptValve("password", "client.json");
+            Console.ForegroundColor = ConsoleColor.Green;
+            //attemptDecryptValve("password", "client.json");
             /*Console.WriteLine("Användning: init <Client Path> <Server Path>");
 
             string clientPath = args[0];
@@ -29,22 +30,47 @@ namespace PasswordManager
             string serverPath = Console.ReadLine();
 
 
+            Console.WriteLine("Använd följande kommandon:");
+            Console.WriteLine("    init             <client> <server> {<pwd>}                             - Skapa en ny klient- och serverfil");
+            Console.WriteLine("    create           <client> <server> {<pwd>} {<secret>}                  - Skapa en ny klient för en existerande server");
+            Console.WriteLine("    get              <client> <server> [<prop>] {<pwd>}                    - Hämta ut lösenord");
+            Console.WriteLine("    set              <client> <server> <prop> [-g] {<pwd>} {<value>}       - Spara ett lösenord");
+            Console.WriteLine("    delete           <client> <server> <prop> {<pwd>}                      - Ta bort ett lösenord");
+            Console.WriteLine("    secret           <client>                                              - Visa Secret Key");
+            Console.WriteLine("    change           <client> <server> {<pwd>} {<new_pwd>}                 - Ändra huvudlösenord");
 
+
+            string command = Console.ReadLine();
+            switch (command)
+            {
+                case "init":
+                    //init();
+                    break;
+                case "create":
+                    //Create();
+                    break;
+                case "get":
+                    //Get();
+                    break;
+                case "set":
+                    //Set();
+                    break;
+                case "delete":
+                    //Delete();
+                    break;
+                case "secret":
+                    //Secret();
+                    break;
+                case "change":
+                    //Change();
+                    break;
+            }
 
 
             //FILVÄGAR
             init(clientPath, serverPath, masterPassword);
 
-
-
-
-            //SECRET KEY
-
-
-
-            //SERVER
-
-
+            attemptDecryptValve("password", "client.json");
 
             Console.WriteLine("STOP");
 
@@ -58,36 +84,40 @@ namespace PasswordManager
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //Kanske ha i projektmappen istället för på Desktop
 
 
-            //HÄR SKER SKAPANDE AV CLIENT OCH SECRET KEY SOM LÄGGS I CLIENT
+            //CLIENT & SECRET KEY
             string clientFile = Path.Combine(desktopPath, clientPath);
             byte[] secretKeyByteArray = GenerateByteArray(16);
             string secretKey = Convert.ToBase64String(secretKeyByteArray);
-            //Console.WriteLine("Your secret key is: " + secretKey);  --> Detta är secret command?
             Dictionary<string, string> secretDict = new Dictionary<string, string>();
             secretDict["secret"] = secretKey;
             string dictJson = JsonSerializer.Serialize(secretDict);
             File.WriteAllText(clientFile, dictJson); // HÄR SPARAS SECRET KEY INUTI CLIENT FILE
                                                      // Your secret key will be printed in plain-text to standard out" - spotta ut secret key i konsol?
 
-
+            //SERVER
             Dictionary<string, string> server = new Dictionary<string, string>();
             string serverFile = Path.Combine(desktopPath, serverPath);
 
+
+            //VAULT KEY
             byte[] vaultKey = makeVaultKey(mstrpwd, secretKeyByteArray);
+
+
+
+            //VAULT
             Dictionary<string, string> vault = new Dictionary<string, string>();
-            vault = Set(vault, "GOOGLE", "password");
+            //vault = Set(vault, "GOOGLE", "password"); 
             string jsonVault = JsonSerializer.Serialize(vault);
 
-            AesClass aes = new AesClass(jsonVault, vaultKey);// Aes-objekt ska inte sparas, men vi måste spara vår IV för att kunna decrypta senare, Vault Key kan genereras på nytt genom Master Password + Secret Key
+            // AES, KRYPTERING OCH LAGRING AV VAULT OCH IV I SERVER FIL
+            AesClass aes = new AesClass(jsonVault, vaultKey);
             string IV = Convert.ToBase64String(aes.IV);
             string encryptedVault = Convert.ToBase64String(aes.encryptedVault);
             server["vault"] = encryptedVault;
             server["iv"] = IV;
-
             string serverJson = JsonSerializer.Serialize(server);
-
-
             File.WriteAllText(serverFile, serverJson);
+
         }
 
 
@@ -127,7 +157,7 @@ namespace PasswordManager
 
         static void Create(string path)
         {
-            Console.WriteLine("Skriv din secretkey: ");
+            Console.Write("Ange din Secret Key: ");
             string secretKey = Console.ReadLine();
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             string newClient = Path.Combine(desktopPath, path);
@@ -142,12 +172,11 @@ namespace PasswordManager
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //Kanske ha i projektmappen istället för på Desktop
             string clientFile = Path.Combine(desktopPath, clientPath);
             string s = File.ReadAllText(clientFile);
-            Console.WriteLine("Hej");
             Dictionary<string, string> secretDictionary = JsonSerializer.Deserialize<Dictionary<string, string>>(s);
-            foreach (var kvp in secretDictionary)
-            {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-            }
+            string scrtKey = secretDictionary["secret"];
+            byte[] scrtKeyByteArray = Convert.FromBase64String(scrtKey);
+
+            byte[] vk = makeVaultKey(mstrpwd, scrtKeyByteArray);
             //kolla så att Value tillsammans med masterpassword dekryptar valvet. Används för alla metoder som kräver masterpassword
             //byte[] vk = makeVaultKey(mstrpwd, scrtkey);
         }
